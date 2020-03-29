d800af7e54d91ec5b18f48baed3637f33d9d14b3
kkalucha <kanavkalucha@gmail.com>
2020-03-29 02:28:50 -0700

    fixed index checking2

--- app.py
+++ app.py
@@ -153,15 +153,15 @@ def modules_route():
 @app.route('/modules/<int:module_number>')
 @login_required
 def module(module_number):
-    if current_user.locked[module_number - 1] or module_number < 1 or module_number > NUM_MODULES:
+    if module_number < 1 or module_number > NUM_MODULES or current_user.locked[module_number - 1]:
         abort(404)
     return render_template('module.jinja2', module=get_user_module(module_number - 1))
 
 @app.route('/modules/<int:module_number>/<int:submodule_number>/<kind>')
 @login_required
 def submodule(module_number, submodule_number, kind):
-    if current_user.locked_sub[module_number - 1][submodule_number - 1] or module_number < 1 or/
-    module_number > NUM_MODULES or submodule_number < 1 or submodule_number > NUM_SUBMODULES[module_number - 1]:
+    if module_number < 1 or module_number > NUM_MODULES or submodule_number < 1 or submodule_number > NUM_SUBMODULES[module_number - 1]/
+    or current_user.locked_sub[module_number - 1][submodule_number - 1]:
         abort(404)
     return render_template('submodule.jinja2', module_number=module_number, submodule_number=submodule_number, kind=kind)
 
